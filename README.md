cf41250df76131a4b4e3e994d9e1402423d59a5f
richteague <teague@mpia.de>
2016-07-06 09:51:10 +0200

    updated readme

--- README.md
+++ README.md
@@ -1,11 +1,29 @@
 # analyseLIME
+
 Scripts to help analyse [LIME](https://github.com/lime-rt/lime) output.
+
+---
+---
+
+## `readLAMDA`
+
+Provides a class to read in the [LAMDA](http://home.strw.leidenuniv.nl/~moldata/) rate files and provide easy access to the variables.
+
+An instance is created with
+
+```python
+ratefile('path/to/file')
+```
+---
+---
+
 
 #### `analyseCube.py`
 Class to read in a LIME datacube and run basic analysis on it.
 
-#### `readLAMDA.py`
-Class to read in a LAMDA collisional rate file and parse all the appropriate information.
+
+
+
 
 #### `readPopulation.py`
 Class to read in LIME's `population.out` files and functions to explore emission and level populations.
--- readLAMDA.py
+++ readLAMDA.py
@@ -1,8 +1,7 @@
-import numpy as np
-
-# Switch between RADEX names and numbers for collision partners.
-        
 def RADEXnames(key):
+
+    # Switch between RADEX names and numbers for collision partners.
+
     if type(key) is list:
         return [RADEXnames(k) for k in key]
     names = np.array(['H2', 'pH2', 'oH2', 'electrons', 'H', 'He', 'H+'])
@@ -12,21 +11,25 @@ def RADEXnames(key):
         for n in range(names.size):
             if names[n] == key:
                 return n+1
-    return 
+    return
 
-# Read in a LAMDA rate file. Very rough for now.
 
 class ratefile:
+
+    # Read in a LAMDA rate file. Very rough for now.
+
+    import numpy as np
+
     def __init__(self, fn):
         with open(fn) as f:
             self.filein = f.readlines()
-                  
+
         self.molecule = self.filein[1].strip()
         self.mu = float(self.filein[3].strip())
         self.nlevels = int(self.filein[5].strip())
         self.ntransitions = int(self.filein[8+self.nlevels].strip())
         self.npartners = int(self.filein[11+self.nlevels+self.ntransitions].strip())
-        
+
         # Read in the partner names and the bounding line values.
         self.partners = []
         self.linestarts = []
@@ -42,17 +45,17 @@ class ratefile:
             self.lineends.append(lineend)
             linestart = lineend
             n += 1
-        
+
         # Read in the energy level structure.
         self.levels = self.filein[7:7+self.nlevels]
         self.levels = np.array([[float(n) for n in levelsrow.strip().split()]
                                  for levelsrow in self.levels]).T
-        
+
         # Read in the radiative transitions.
         self.transitions = self.filein[10+self.nlevels:10+self.nlevels+self.ntransitions]
         self.transitions = np.array([[float(n) for n in transrow.strip().split()]
                                       for transrow in self.transitions]).T
-                                      
+
         # Split into appropriate arrays.
         self.deltaE = self.levels[1]
         self.weights = self.levels[2]
@@ -60,8 +63,5 @@ class ratefile:
         self.EinsteinA = self.transitions[3]
         self.frequencies = self.transitions[4] * 1e9
         self.E_upper = self.transitions[5]
-                                      
+
         return
-
-
-
