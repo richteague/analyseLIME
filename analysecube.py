85652b25aa146528cb244c21b1d825dc21317c9e
richteague <teague@mpia.de>
2016-09-26 06:48:05 -0400

    unit conversion ok

--- analysecube.py
+++ analysecube.py
@@ -8,7 +8,7 @@ from matplotlib.patches import Ellipse
 
 class beamclass:
     """Beam object to pass to convolution functions."""
-    
+
     def __init__(self, beamparams):
 
         if type(beamparams) is float:
@@ -28,17 +28,17 @@ class beamclass:
             tmp = self.min
             self.min = self.maj
             self.maj = tmp
-            
+
         self.pa = np.radians(self.pa % 360.)
         self.area = np.pi * self.min * self.maj / 4. / np.log(2)
-        
+
         return
 
 
 class cubeclass:
     """Datacube read in from LIME."""
-    
-    def __init__(self, path, dist=None, inc=None, pa=None, 
+
+    def __init__(self, path, dist=None, inc=None, pa=None,
                  quick_convolve=True):
         """Read in the data and calculate the cube axes."""
         self.filename = path
@@ -67,7 +67,7 @@ class cubeclass:
         else:
             self.dist = dist
             self.projaxes = self.posax * self.dist
-            
+
         if pa is None:
             try:
                 self.pa = getval(self.filename, 'pa', 0)
@@ -83,26 +83,24 @@ class cubeclass:
         self.y0 = getval(self.filename, 'crpix2', 0) - 1.
         self.nchan = self.velax.size
         self.npix = self.posax.size
-        
+
         # Parse the brightness unit.
-        self.bunit = fits.getval(self.filename, 'bunit', 0).lower()
-        self.bunit = self.bunit.replace('/', '')
-        self.bunit = self.bunit.replace('per', '')
-        self.bunit = self.replace(' ', '')
-        if self.bunit not in ['jypixel', 'mjypixel', 'k']:
-            raise ValueError('Cannot read brightness unit.')
-        
+        # Should make it easier to calculate converions.
+        self.bunit, self.tounit = self.brightnessconversions()
+
         self.x = self.posax[None, :]
         self.y = self.posax[:, None] / np.cos(self.inc)
         self.pixscale = np.diff(self.posax)[0]
         self.rvals = np.hypot(self.x, self.y)
         self.convolved_cubes = {}
-        self.convolved_zeroths = {}  
+        self.convolved_zeroths = {}
         return
-    
+
+
     def pixtobeam(self, beam):
         """Jy/beam = Jy/pix * pixtobeam()"""
         return beam.area / np.diff(self.posax)[0]**2
+
 
     def beamKernel(self, beam):
         """Generate a beam kernel for the convolution."""
@@ -123,7 +121,7 @@ class cubeclass:
     def convolveChannel(self, channel, beam):
         """Returns the convolved channel or moment map with the beam."""
         return convolve(channel, self.beamKernel(beam))
-        
+
         self.convolved_zeroths[beam_maj, beam_min, beam_pa] = conv_zeroth
         return self.convolved_zeroths[beam_maj, beam_min, beam_pa]
 
@@ -137,7 +135,7 @@ class cubeclass:
         cube = [self.convolveChannel(chan, beam) for chan in self.data]
         self.convolved_cubes[beam_maj, beam_min, beam_pa] = np.array(cube)
         return self.convolved_cubes[beam_maj, beam_min, beam_pa]
-        
+
 
     def getPositionAxis(self):
         """Returns the position axis in arcseconds."""
@@ -160,23 +158,23 @@ class cubeclass:
         nu = getval(self.filename, 'restfreq', 0)
         return self.getVelocityAxis() * nu / sc.c
 
-    
+
     def clipData(self, low=0, high=-1, removeCont=1, beam=None):
         """Clip the datacube to specific channels and remove continuum. If a
         list of beam parameters are specified, use the convolved cube instead.
         Continuum subtraction just models the first channel as continuum."""
-        
+
         if beam is None or self.quick_convolve:
             data = self.data.copy()
         else:
             data = self.convolveCube(beam)
-            
+
         if removeCont:
             rgn = range(low, self.nchan+1)[:high]
             data = np.array([data[i]-data[0] for i in rgn])
         else:
             data = data[low:high]
-            
+
         return np.where(data >= 0., data, 0.)
 
 
@@ -189,42 +187,58 @@ class cubeclass:
             velo /= 1e3
         return np.array(velo)
 
-    
-    def getZeroth(self, low=0, high=-1, removeCont=1, bunit=None, 
+
+    def getZeroth(self, low=0, high=-1, removeCont=1, bunit=None,
                   vunit='km/s', mask=True, beamparams=None):
         """Returns the zeroth moment map. Convolve if necessary."""
         if beamparams is not None:
             beam = beamclass(beamparams)
         else:
             beam = None
-            
-        # Calculate the zeroth moment. If not quick_convolve, convolve the
-        # entire datacube. 
+        # Calculate a quick zeroth momenet. TODO: Include a dictionary here.
         velo = self.clipVelo(low=low, high=high, vunit=vunit)
         data = self.clipData(low=low, high=high, removeCont=removeCont, beam=beam)
         data *= self.convertBunit(bunit, beam=beam)
         zeroth = np.trapz(data, dx=abs(np.diff(velo)[0]), axis=0)
-        
-        # If quick_convolve, only convolve the zeroth moment. 
+        # If quick_convolve, only convolve the zeroth moment, else the whole cube.
         if (beam is not None and self.quick_convolve):
             try:
-                zeroth = self.convolved_zeroths[beam.maj, beam.min, beam.pa]
+                zeroth = self.convolved_zeroths[beam.maj, beam.min, beam.pa, bunit.lower()]
             except:
                 zeroth = self.convolveChannel(zeroth, beam)
-                self.convolved_zeroths[beam.maj, beam.min, beam.pa] = zeroth
-                
-        # Masked zeroth moment.
+                self.convolved_zeroths[beam.maj, beam.min, beam.pa, bunit.lower()] = zeroth
+        # Return a masked, or unmasked, zeroth moment.
         if mask:
             return zeroth * self.getMask()
         else:
             return zeroth
 
+    def getZerothProfile(self, bins=None, nbins=50, low=0, high=-1,
+                         removeCont=1, bunit=None, vunit='km/s', mask=True,
+                         beamparams=None):
+        """Get the radial profile of the zeroth moment."""
+        # Generate the radial grid if not specified.
+        if bins is None:
+            bins = np.linspace(0, 1.2*self.posax.max(), nbins+1)
+        else:
+            nbins = len(bins)-1
+        ridxs = np.digitize(self.rvals, bins).ravel()
+        # Return the zeroth moment with requested parameters.
+        zeroth = self.getZeroth(low=low, high=high, removeCont=removeCont,
+                                bunit=bunit, vunit=vunit, mask=mask,
+                                beamparams=beamparams).ravel()
+        # Calculate the mean and standard devation of each radial bin.
+        avg = [np.nanmean(zeroth[ridxs == r]) for r in range(1, nbins+1)]
+        std = [np.nanstd(zeroth[ridxs == r]) for r in range(1, nbins+1)]
+        rad = np.average([bins[1:], bins[:-1]], axis=0)
+        return np.array([rad, avg, std])
 
-    def getFirst(self, low=0, high=-1, removeCont=1,
-                 method=1, vunit='km/s', mask=True, beam=None):
+
+    def getFirst(self, low=0, high=-1, removeCont=1, method=1, vunit='km/s',
+                 mask=True, beam=None):
         """Calculates the first moment by two methods: 1, intensity weighted;
         2, velocity of maximum emission."""
-        
+
         velo = self.clipVelo(low=low, high=high, vunit=vunit)
         data = self.clipData(low=low, high=high, removeCont=removeCont,
                              beam=beam)
@@ -249,11 +263,11 @@ class cubeclass:
     def getSecond(self, low=0, high=-1, removeCont=1,
                   vunit='km/s', mask=True, beam=None):
         """Returns the second moment map."""
-        
+
         velo = self.clipVelo(low=low, high=high, vunit=vunit)
         data = self.clipData(low=low, high=high, removeCont=removeCont,
                              beam=beam)
-        first = self.getFirst(low=low, high=high, method=1, vunit=vunit, 
+        first = self.getFirst(low=low, high=high, method=1, vunit=vunit,
                               mask=False)
         vcube = velo[:, None, None] * np.ones(data.shape)
         fcube = first[None, :, :] * np.ones(data.shape)
@@ -278,26 +292,7 @@ class cubeclass:
             self.nanmask = np.where(mask == mask.min(), np.nan, 1)
         return self.nanmask
 
-    def getZerothProfile(self, bins=None, nbins=50, lowchan=0,
-                         highchan=-1, removeCont=1, bunit=None,
-                         vunit='km/s', mask=True, beamparams=None):
-        """Get the radial profile of the zeroth moment."""                        
-        if bins is None:
-            bins = np.linspace(0, 1.2*self.posax.max(), nbins+1)
-        else:
-            nbins = len(bins)-1
-        ridxs = np.digitize(self.rvals, bins).ravel()
-        zeroth = self.getZeroth(low=lowchan, 
-                                high=highchan, 
-                                removeCont=removeCont, 
-                                bunit=bunit, 
-                                vunit=vunit, 
-                                mask=mask, 
-                                beamparams=beamparams).ravel()
-        avg = [np.nanmean(zeroth[ridxs == r]) for r in range(1, nbins+1)]
-        std = [np.nanstd(zeroth[ridxs == r]) for r in range(1, nbins+1)]
-        rad = np.average([bins[1:], bins[:-1]], axis=0)
-        return np.array([rad, avg, std])
+
 
     # Get the Radial profile of the second moment.
     def getSecondProfile(self, bins=None, nbins=50, lowchan=0,
@@ -320,56 +315,63 @@ class cubeclass:
         rad = np.average([bins[1:], bins[:-1]], axis=0)
         return np.array([rad, avg, std])
 
+    def brightnessconversions(self):
+        """Calculate the conversion brightness unit factors."""
+
+        # Brightnes unit of file.
+        bunit = fits.getval(self.filename, 'bunit', 0).lower()
+        bunit = bunit.replace('/', '').replace('per', '').replace(' ', '')
+        if bunit not in ['jypixel', 'k']:
+            raise ValueError('Cannot read brightness unit.')
+
+        # Conversion dictionary.
+        tounit = {}
+        if bunit == 'jypixel':
+            tounit['jypixel'] = 1.
+            tounit['mjypixel'] = 1e3
+            tounit['k'] = self.JanskytoKelvin()
+        else:
+            tounit['k'] = 1.
+            tounit['jypixel'] = 1. / self.JanskytoKelvin()
+            tounit['mjypixel'] = 1e-3 / self.JanskytoKelvin()
+
+        return bunit, tounit
+
     def convertBunit(self, bunit, beam=None):
-        """Convert the brightness units from self.bunit to bunit."""
+        """Convert the brightness units to bunit."""
         if bunit is None:
             return 1.
         bunit = bunit.lower().replace('/', '')
         bunit = bunit.replace('per', '').replace(' ', '')
         if bunit not in ['jybeam', 'mjybeam', 'jypixel', 'mjypixel', 'k']:
             raise ValueError("bunit must be (m)Jy/pixel, (m)Jy/beam or K.")
-            
-        rescale = self.rescaling[bunit]
-        if 'beam' in bunit:
-            rescale *= self.pixtobeam(beam)    
-        return 
-        
-        
-        # Apply the appropriate scaling.
-        if (self.bunit == bunit or bunit is None):
-            print 'No change: %s is %s.' % (self.bunit, bunit)
-            scale = 1.
-        elif ('mjy' in bunit.lower() and not 'beam' in bunit.lower()):
-            print 'From %s to mJy/pix (asked for %s).' % (self.bunit, bunit)
-            scale = 1e3 / self.JanskytoKelvin()
-        elif ('mjy' in bunit.lower() and 'beam' in bunit.lower()):
-            print 'From %s to mJy/beam (asked for %s).' % (self.bunit, bunit)
-            scale = 1e3 / self.JanskytoKelvin() * self.pixtobeam(beam)
-        elif ('jy' in bunit.lower() and not 'beam' in bunit.lower()):
-            print 'From %s to Jy/pix (asked for %s).' % (self.bunit, bunit)
-            scale = self.JanskytoKelvin()**-1
-        elif ('jy' in bunit.lower() and 'beam' in bunit.lower()):
-            scale = self.pixtobeam(beam) / self.JanskytoKelvin()
-            print 'From %s to Jy/beam (asked for %s).' % (self.bunit, bunit)
-        elif 'k' in bunit.lower():
-            print 'From %s to K (asked for %s).' % (self.bunit, bunit)
-            scale = self.JanskytoKelvin()
-        else:
-            raise NotImplementedError("Unknown brightness unit.")
-        return scale
+        # Convert to requested unit.
+        if bunit == 'k':
+            return self.tounit['k']
+        elif 'mjy' in bunit:
+            rescale = self.tounit['mjypixel']
+        elif 'jy' in bunit:
+            rescale = self.tounit['jypixel']
+        # Add the beam rescaling.
+        if ('beam' in bunit and beam is not None):
+            rescale *= self.pixtobeam(beam)
+        elif ('beam' in bunit and beam is None):
+            print 'Warning, no beam specified! Reverting to per pixel.'
+        return rescale
 
-    # Jansky to Kelvin conversion.
-    def JanskytoKelvin(self):
+
+    def JanskytoKelvin(self, beam=None):
+        """Jansky to Kelvin conversion. If no beam specified will return Jy/pix,
+        else will return Jy/beam."""
         jy2k = 10**-26 * sc.c**2.
         jy2k /= 2. * fits.getval(self.filename, 'restfreq', 0)**2.
-        jy2k /= sc.k * np.radians(fits.getval(self.filename, 'cdelt2', 0))**2.
+        jy2k /= sc.k
+        if beam is None:
+            jy2k /= np.radians(fits.getval(self.filename, 'cdelt2', 0))**2.
+        else:
+            jy2k /= beam
         return jy2k
 
-    # Plot the outline of the disk.
-    def plotOutline(self, ax, level=0.99, lw=.5, c='k', ls='-'):
-        ax.contour(self.posax, self.posax, self.getMask(), [level],
-                   linewidths=[lw], colors=[c], linestyles=[ls])
-        return
 
     def getIntegratedIntensity(self, removeCont=1, lowchan=0,
                                highchan=-1, bunit='jy', vunit='km/s'):
@@ -382,20 +384,27 @@ class cubeclass:
         velo = self.clipVelo(lowchan=lowchan, highchan=highchan,
                              vunit=vunit)
         return velo, np.array([np.sum(c) for c in data])
-        
-        
+
+
     def plotBeam(self, ax, beamparams, x=0.1, y=0.1, color='k'):
         """Plot the beam on the supplied axes."""
         beam = beamclass(beamparams)
         x_pos = x * (ax.get_xlim()[1] - ax.get_xlim()[0]) + ax.get_xlim()[0]
         y_pos = y * (ax.get_ylim()[1] - ax.get_ylim()[0]) + ax.get_ylim()[0]
-        ax.add_patch(Ellipse((x_pos, y_pos), 
+        ax.add_patch(Ellipse((x_pos, y_pos),
                              width = beam.min,
                              height = beam.maj,
                              angle = np.degrees(beam.pa),
-                             fill = False, 
-                             hatch = '////', 
-                             lw = 1.25, 
+                             fill = False,
+                             hatch = '////',
+                             lw = 1.25,
                              color = color,
                              transform = ax.transData))
+        return
+
+
+    def plotOutline(self, ax, level=0.99, lw=.5, c='k', ls='-'):
+        """Plot the outline of the emission on teh supplied axes."""
+        ax.contour(self.posax, self.posax, self.getMask(), [level],
+                   linewidths=[lw], colors=[c], linestyles=[ls])
         return
